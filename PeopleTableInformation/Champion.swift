4401d8416ccd8a71f61c59d567039d37d670daf7
DBors17 <dorin.f17@gmail.com>
2025-04-10 11:02:30 -0700

    scroll

--- PeopleTableInformation/Champion.swift
+++ PeopleTableInformation/Champion.swift
@@ -9,13 +9,15 @@ import Foundation
 
 class Champion{
     var name, position, ultimate, role, image, url: String;
+    var gallery : [String];
     
-    init(name: String, position: String, ultimate: String, role: String, image: String, url: String){
+    init(name: String, position: String, ultimate: String, role: String, image: String, url: String, gallery: [String]){
         self.name = name
         self.position = position
         self.ultimate = ultimate
         self.role = role
         self.image = image
         self.url = url
+        self.gallery = gallery
     }
 }
